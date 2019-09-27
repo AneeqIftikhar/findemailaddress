48c3805e11228ee935d4a0c0fbe51e7ccaa3812b
DESKTOP-DAPVUPS\BudDev - 016 <kh.aneeq@gmail.com>
2019-09-28 02:42:24 +0500

    Zeor Credit Checks

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -14,6 +14,7 @@ use App\Rules\BlackListDomains;
 use App\TwoCheckout\TwoCheckoutApi;
 use App\FastSpring\FastSpringApi;
 use Ramsey\Uuid\Uuid;
+use Illuminate\Support\Facades\DB;
 class RegisterController extends Controller
 {
     /*
@@ -74,48 +75,58 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
 
+        DB::beginTransaction();
+        try
+        {
+            $free_package=Package::where('name','free')->first();
+            $data['password']=Hash::make($data['password']);
+            $data['credits']=$free_package->credits;
+            $data['user_uuid'] = Uuid::uuid4();
+            $data['package_id'] = $free_package->id;
+            $user = new User($data);
+            $user->save();
+            $FastSpringApi=new FastSpringApi();
+            $name=explode(" ",$user->name);
+            if(count($name)>2)
+            {
+                $first_name=$name[0];
+                $last_name=$name[count($name)-1];
+            }
+            else if (count($name)>1)
+            {
+                $first_name=$name[0];
+                $last_name=$name[1];
+            }
+            else
+            {
+                $first_name=$name[0];
+                $last_name=" ";
+            }
+            $if_exists=$FastSpringApi->getCustomerUsingEmail($user->email);
+            if($if_exists['result']=="error")
+            {
+                $account=$FastSpringApi->createCustomer($first_name,$last_name,$user->email,$user->user_uuid);
+                $user->payment_user_reference=$account['id'];
+            }
+            else
+            {
+                $user->payment_user_reference=$if_exists['accounts'][0]['id'];
+                
+            }
+            $user->save();
+            $user_package_log = new UserPackagesLogs;
+            $user_package_log->package()->associate($free_package);
+            $user_package_log->user()->associate($user);
+            $user_package_log->save();
+            DB::commit();
+            return $user;
 
-        $free_package=Package::where('name','free')->first();
-        $data['password']=Hash::make($data['password']);
-        $data['credits']=$free_package->credits;
-        $data['user_uuid'] = Uuid::uuid4();
-        $data['package_id'] = $free_package->id;
-        $user = new User($data);
-        $user->save();
-        $FastSpringApi=new FastSpringApi();
-        $name=explode(" ",$user->name);
-        if(count($name)>2)
-        {
-            $first_name=$name[0];
-            $last_name=$name[count($name)-1];
         }
-        else if (count($name)>1)
+        catch(\Exception $e)
         {
-            $first_name=$name[0];
-            $last_name=$name[1];
+            DB::rollback();
+            throw $e;
         }
-        else
-        {
-            $first_name=$name[0];
-            $last_name=" ";
-        }
-        $if_exists=$FastSpringApi->getCustomerUsingEmail($user->email);
-        if($if_exists['result']=="error")
-        {
-            $account=$FastSpringApi->createCustomer($first_name,$last_name,$user->email,$user->user_uuid);
-            $user->payment_user_reference=$account['id'];
-        }
-        else
-        {
-            $user->payment_user_reference=$if_exists['accounts'][0]['id'];
-            
-        }
-        $user->save();
-        $user_package_log = new UserPackagesLogs;
-        $user_package_log->package()->associate($free_package);
-        $user_package_log->user()->associate($user);
-        $user_package_log->save();
-
-        return $user;
+        
     }
 }
