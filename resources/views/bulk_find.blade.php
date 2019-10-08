0e97318a44f2c0130da8293946b4a507dad18db7
DESKTOP-DAPVUPS\BudDev - 016 <kh.aneeq@gmail.com>
2019-10-09 04:17:48 +0500

    Bulk Feature started

--- resources/views/bulk_find.blade.php
+++ resources/views/bulk_find.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header"><h3>Batch Find Email</h3></div>
+                <div class="card-header"><h3>Bulk Find Email</h3></div>
 
                 <div class="card-body">
                 	@if ($errors->any())
