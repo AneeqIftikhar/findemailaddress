58b881baff56c3833b3d7768bb83b0afa1f3813a
aneeqiftikhar <kh.aneeq@gmail.com>
2020-05-18 00:21:38 +0500

    Cors

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -13,7 +13,7 @@ class AdminController extends Controller
     public function __construct()
     {
         $this->middleware('auth:api', ['except' => ['login']]);
-        $this->middleware('cors', ['except' => ['login']]);
+//        $this->middleware('cors', ['except' => ['login']]);
     }
     public function login(Request $request)
     {
--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,6 +15,24 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
+
+        header('Access-Control-Allow-Origin', '*');
+        $headers = [
+            'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization',
+
+        ];
+        if($request->getMethod()=="OPTIONS")
+        {
+            return response()->json('OK',200,$headers);
+        }
+        $response = $next($request);
+        foreach ($headers as $key=>$value)
+        {
+            $response->header($key,$value);
+        }
+        return $response;
+
         return $next($request)
           ->header('Access-Control-Allow-Origin', '*')
           ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
