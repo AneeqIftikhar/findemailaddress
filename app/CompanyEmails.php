16f63727ef5cf1e9e65fd8a92e0af81c248f7b63
Khawaja Hamza <hamzarazzaq96@gmail.com>
2021-02-28 17:12:15 +0500

    Updated CompanyEmails Model

--- app/CompanyEmails.php
+++ app/CompanyEmails.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class CompanyEmails extends Model
 {
     protected $table = 'company_emails';
-    protected $fillable = ['user_id', 'company_slug', 'people_slug', 'created_at', 'updated_at'];
+    protected $fillable = ['user_id', 'company_id', 'people_id', 'created_at', 'updated_at'];
 }
